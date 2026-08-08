61d96c51cf047d7a94ca7d104dda2ffb3a92cf01
stretchingon <keh7627@gmail.com>
2026-08-08 19:19:06 +0900

    Update send-alimtalk.js

--- api/send-alimtalk.js
+++ api/send-alimtalk.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   }
 
   if (req.method !== 'POST') {
-    return res.status(405).json({ success: false, error: 'Method not allowed' });
+    return res.status(200).json({ success: false, error: 'Method not allowed' });
   }
 
   try {
