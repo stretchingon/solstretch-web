d7ef2aaf26db699141b2cc51b2de46ee5285720b
stretchingon <keh7627@gmail.com>
2026-08-08 21:25:28 +0900

    Update send-alimtalk.js

--- api/send-alimtalk.js
+++ api/send-alimtalk.js
@@ -1,8 +1,6 @@
-```js
-export default function handler(req, res) {
+module.exports = (req, res) => {
   res.status(200).json({
     success: true,
     message: 'Vercel API 정상 작동!'
   });
-}
-```
+};
